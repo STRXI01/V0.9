5749f69ff5d3fcec552c6d916635aa04de625b97
koreansmu <shuklakusum4q@gmail.com>
2025-11-22 12:19:04 +0530

    Update userbot.py

--- Syphix/core/userbot.py
+++ Syphix/core/userbot.py
@@ -14,35 +14,35 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="AMBOTAss1",
+            name="Cipher1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
-            name="AMBOTAss2",
+            name="Cipher2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
-            name="AMBOTAss3",
+            name="Cipher3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
-            name="AMBOTAss4",
+            name="Cipher4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
-            name="AMBOTAss5",
+            name="Cipher5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
