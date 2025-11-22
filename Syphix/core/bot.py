4472c7d02a5fa8464cb8e5b05e9dba23d4a41314
koreansmu <shuklakusum4q@gmail.com>
2025-11-22 12:14:15 +0530

    Update bot.py

--- Syphix/core/bot.py
+++ Syphix/core/bot.py
@@ -10,7 +10,7 @@ import config
 from ..logging import LOGGER
 
 
-class AMBOT(Client):
+class Cipher(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
